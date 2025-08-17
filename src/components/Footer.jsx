6accf7f12946b3cd1d7c3c26e04cb5b1366345b9
清贤 <666206@gmail.com>
2025-08-17 18:07:41 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,9 @@ export default function Footer() {
   return (
     <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
       <div >
-        <p className="text-xs text-gray-500">Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。 本程序基于Cloudflare Pages，开源于
-          <Link 
-          href="https://github.com/x-dr/telegraph-Image"
-          className="text-blue-300  hover:text-red-900 ml-1"
-          target="_blank"
-          rel="noopener noreferrer"
-          >GitHub Telegraph-Image</Link> </p>
+        <p className="text-xs text-gray-500">Copyright © 2025 王小二. All rights reserved. Made with Love. 
+          请勿上传违反中国法律的图片，违者后果自负，本站不承担任何责任。
+        </p>
       </div>
     </footer>
   );
