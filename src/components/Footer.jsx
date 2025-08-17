43e97f38151dd4fd70391215c7e016178a6565aa
清贤 <666206@gmail.com>
2025-08-17 18:14:39 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,13 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 export default function Footer() {
   return (
-    <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
-      <div >
-        <p className="text-xs text-gray-500">Copyright © 2025 王小二. All rights reserved. Made with Love. 
-          请勿上传违反中国法律的图片，违者后果自负，本站不承担任何责任。
+    <footer className="w-full bg-slate-200 py-4">
+      <div className="mx-auto text-center space-y-1">
+        <p className="text-xs text-gray-500">
+          Copyright © 2025 王小二. All rights reserved. Made with Love.
+        </p>
+        <p className="text-xs text-gray-500">
+          请勿上传违反中国法律的图片，违者后果自负，本站不承担任何法律责任。
         </p>
       </div>
     </footer>
